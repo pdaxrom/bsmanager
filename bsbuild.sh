4a15b1b15a2776665b6e63d0bc84408a97d99dbd
sashz <23256427+pdaxrom@users.noreply.github.com>
2015-07-23 06:15:14 +0000

    build tarball

--- bsbuild.sh
+++ bsbuild.sh
@@ -164,8 +164,8 @@ download http://isl.gforge.inria.fr/isl-0.15.tar.xz
 download http://bugseng.com/products/ppl/download/ftp/releases/1.1/ppl-1.1.tar.xz
 download http://www.bastoul.net/cloog/pages/download/cloog-0.18.3.tar.gz
 #download http://www.bastoul.net/cloog/pages/download/cloog-parma-0.16.1.tar.gz
-download http://ftp.gnu.org/gnu/binutils/binutils-2.25.1.tar.bz2
-download http://gcc.cybermirror.org/releases/gcc-4.9.3/gcc-4.9.3.tar.bz2
+download http://ftp.gnu.org/gnu/binutils/binutils-${TARGET_BINUTILS_VERSION}.tar.bz2
+download http://gcc.cybermirror.org/releases/gcc-${TARGET_GCC_VERSION}/gcc-${TARGET_GCC_VERSION}.tar.bz2
 
 build gmp-6.0.0a.tar.xz "--enable-cxx --disable-shared" "" gmp-6.0.0
 build mpfr-3.1.3.tar.xz "--disable-shared"
@@ -174,14 +174,56 @@ build isl-0.15.tar.xz "--disable-shared"
 build ppl-1.1.tar.xz "--disable-shared --with-gmp=$INST_PREFIX"
 build cloog-0.18.3.tar.gz "--disable-shared"
 #build cloog-parma-0.16.1.tar.gz "--disable-shared"
-build binutils-2.25.1.tar.bz2 "--target=$TARGET_ARCH --host=$(uname -m)-linux-gnu --build=$(uname -m)-linux-gnu \
---with-sysroot=$TARGET_SYSROOT --disable-nls --disable-werror --disable-shared"
-build gcc-4.9.3.tar.bz2 "--target=$TARGET_ARCH --host=$(uname -m)-linux-gnu --build=$(uname -m)-linux-gnu \
---with-sysroot=$TARGET_SYSROOT --disable-nls --disable-werror --disable-shared --disable-bootstrap \
+build binutils-${TARGET_BINUTILS_VERSION}.tar.bz2 "--target=$TARGET_ARCH --host=$(uname -m)-linux-gnu --build=$(uname -m)-linux-gnu \
+--with-sysroot=$TARGET_SYSROOT --disable-nls --disable-werror"
+build gcc-${TARGET_GCC_VERSION}.tar.bz2 "--target=$TARGET_ARCH --host=$(uname -m)-linux-gnu --build=$(uname -m)-linux-gnu \
+--with-sysroot=$TARGET_SYSROOT --disable-nls --disable-werror --enable-shared --disable-bootstrap \
 --with-gmp=$INST_PREFIX --with-mpfr=$INST_PREFIX --with-mpc=$INST_PREFIX --with-cloog=$INST_PREFIX --with-isl=$INST_PREFIX --with-ppl=$INST_PREFIX \
 --disable-ppl-version-check --disable-cloog-version-check --disable-isl-version-check --enable-cloog-backend=isl \
 --enable-languages=c,c++ --enable-linker-build-id --enable-threads=posix \
 --enable-libstdcxx-debug --enable-libstdcxx-time=yes --enable-gnu-unique-object --enable-plugin \
 --disable-sjlj-exceptions $GCC_CONFIG_FLAGS"
 
-#
+# prepare crosscompiler pack
+
+PKG_DIR=/tmp/rootfs$$
+
+mkdir -p ${PKG_DIR}/${INST_PREFIX}
+
+cp -R ${INST_PREFIX}/* ${PKG_DIR}/${INST_PREFIX}/
+
+rm -rf ${PKG_DIR}/${INST_PREFIX}/include ${PKG_DIR}/${INST_PREFIX}/lib/cloog-isl ${PKG_DIR}/${INST_PREFIX}/lib/isl \
+    ${PKG_DIR}/${INST_PREFIX}/lib/pkgconfig ${PKG_DIR}/${INST_PREFIX}/lib/lib*.a ${PKG_DIR}/${INST_PREFIX}/lib/lib*.la \
+    ${PKG_DIR}/${INST_PREFIX}/bin/cloog ${PKG_DIR}/${INST_PREFIX}/bin/ppl* \
+    ${PKG_DIR}/${INST_PREFIX}/share/aclocal ${PKG_DIR}/${INST_PREFIX}/share/doc ${PKG_DIR}/${INST_PREFIX}/share/info
+
+find ${PKG_DIR}/${INST_PREFIX}/share/man -name "ppl*" -o -name "libppl*" | xargs rm -f
+
+strip ${PKG_DIR}/${INST_PREFIX}/bin/* ${PKG_DIR}/${INST_PREFIX}/${TARGET_ARCH}/bin/* \
+    ${PKG_DIR}/${INST_PREFIX}/libexec/gcc/${TARGET_ARCH}/${TARGET_GCC_VERSION}/* \
+    ${PKG_DIR}/${INST_PREFIX}/libexec/gcc/${TARGET_ARCH}/${TARGET_GCC_VERSION}/install-tools/* \
+    ${PKG_DIR}/${INST_PREFIX}/libexec/gcc/${TARGET_ARCH}/${TARGET_GCC_VERSION}/plugin/*
+
+cat > ${PKG_DIR}/${INST_PREFIX}/bin/${TARGET_ARCH}-sysroot-path << EOF
+#!/bin/sh
+if test "\$1" == "--install"; then
+    ARCH=\$(uname -i)
+    if test "\$ARCH" = ""; then
+	ARCH=\$(uname -m)
+    fi
+    if test "\$ARCH" != $(uname -m); then
+	mkdir -p $(dirname $TARGET_SYSROOT)
+	ln -sf / $TARGET_SYSROOT
+    else
+	echo "Please, install sysroot to $TARGET_SYSROOT"
+    fi
+else
+    echo $TARGET_SYSROOT
+fi
+EOF
+
+chmod 755 ${PKG_DIR}/${INST_PREFIX}/bin/${TARGET_ARCH}-sysroot-path
+
+tar Jcf ${TOPDIR}/${TARGET_ARCH}-gcc-${TARGET_GCC_VERSION}-$(uname -m)${PACKAGE_ID}.tar.xz -C ${PKG_DIR} .
+
+rm -rf ${PKG_DIR}
