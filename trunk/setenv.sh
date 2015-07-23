5af3f2c22d0a37f2a9a01e6e69f01aac5fb29aa3
sash <sash@8b1e7bd0-1913-4b52-b24b-3cbb6da9a59d>
2015-07-23 06:15:14 +0000

    build tarball

--- trunk/setenv.sh
+++ trunk/setenv.sh
@@ -4,6 +4,10 @@ INST_PREFIX="/opt/madisa/toolchain"
 
 TARGET_ARCH=arm-linux-gnueabihf
 TARGET_SYSROOT="/opt/madisa/rootfs/raspbian"
+TARGET_GCC_VERSION=4.9.3
+TARGET_BINUTILS_VERSION=2.25.1
+
+PACKAGE_ID="-raspbian"
 
 GCC_CONFIG_FLAGS="--with-arch=armv6 --with-fpu=vfp --with-float=hard"
 
